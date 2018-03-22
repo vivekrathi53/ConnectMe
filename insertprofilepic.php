034f0c21a9db17857fff9f08456353dc5bf1c0c8
Vivek Rathi <vivekrathi53@gmail.com>
2018-03-22 15:53:57 +0530

    Major bug fixed
    profile picture can be inserted smoothly

--- insertprofilepic.php
+++ insertprofilepic.php
@@ -1,10 +1,13 @@
 <?php
 	session_start();
+	$location="C:\wamp64\www\My Project\profileimages";
 	$currentuser=$_SESSION['username'];
-	$tempimage=basename($_FILES['profilepic']['tmp_name']);
-	$k=move_uploaded_file($tempimage, "images123/".$currentuser.".jpg");
+	$tempimage=$_FILES['profilepic']['tmp_name'];
+    $imagename=$_FILES['profilepic']['name'];
+	$k=move_uploaded_file($tempimage,"$imagename");
+	if(!$k) echo "problem";
 	$con=mysqli_connect('localhost','root','password','connectme');
-	$sql="UPDATE peopleinfo SET Profilepic='$currentuser.jpg' WHERE Username='$currentuser'";
+	$sql="UPDATE peopleinfo SET Profilepic='$imagename' WHERE Username='$currentuser'";
 	$run=mysqli_query($con,$sql);
 	if($run)
 		if($k)
